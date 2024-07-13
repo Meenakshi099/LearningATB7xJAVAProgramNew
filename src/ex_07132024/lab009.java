82f829aa7325a1bb72fc4817b48191ed40810d1c
Meenakshi G <meenuhari082@gmail.com>
2024-07-13 12:51:33 +0530

    commit

---
+++ src/ex_07132024/lab009.java
@@ -0,0 +1,11 @@
+package ex_07132024;
+
+public class lab009 {
+
+    public static void main(String[] args) {
+        String name1 ="Meenu";
+        String name2="Meenakshi";
+        System.out.println( name1 +" "+ name2);
+    }
+
+}

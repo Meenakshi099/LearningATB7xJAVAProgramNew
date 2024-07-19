3be51690e45848f9d5ee0c7117256717ed6361c3
Meenakshi G <meenuhari082@gmail.com>
2024-07-19 23:00:06 +0530

    commit

---
+++ src/ex_14072024/lab032.java
@@ -0,0 +1,19 @@
+package ex_14072024;
+
+public class lab032 {
+    public static void main(String[] args) {
+        String name = "Pramod"; // Assignment Operators
+        String name2 = new String("Pramod"); // New Operator
+        System.out.println(name == name2);
+        String s1 ="hello";
+        String s2="Hi";
+        String s3 = s1;
+        System.out.println(s1 == s3);
+
+        // Both of them, actually save the name, name2 in different ways JVM
+        // How they store the values in the JVM?
+
+
+        // SCP nad Heap - JVM
+    }
+}
